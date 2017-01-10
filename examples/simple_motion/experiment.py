68f57a94b14e86ae52d832b4ab82dfe16c93ac66
mwaskom <mwaskom@stanford.edu>
2017-01-10 14:39:36 -0500

    Start writing simple example code

---
+++ examples/simple_motion/experiment.py
@@ -0,0 +1,62 @@
+import numpy as np
+import pandas as pd
+
+from visigoth.utils import flexible_values
+from visigoth.stimuli import Point, Points, GratingStim
+
+
+def create_stimuli(exp):
+
+    fix = Point(exp.win, exp.p.fix_radius)
+    targets = Points(exp.win, exp.p.target_pos, exp.p.target_radius)
+    grating = GratingStim(exp.win,
+                          sf=exp.p.stim_sf,
+                          tex=exp.p.stim_tex,
+                          mask=exp.p.stim_mask,
+                          size=exp.p.stim_size,
+                          contrast=exp.p.stim_contrast,
+                          pos=(0, 0),
+                          ori=0,
+                          autoLog=False)
+
+    return dict(fix=fix, targets=targets, grating=grating)
+
+
+def generate_trials(exp):
+
+    for t in range(1, exp.p.n_trials + 1):
+
+        now = exp.clock.getTime()
+        iti = flexible_values(exp.p.iti_dur)
+        trial_time = now + iti
+
+        motion = flexible_values([-1, 1])
+        target = int(motion > 1)
+
+        trial_info = dict(
+
+            subject=exp.p.subject,
+            session=exp.p.date,
+            run=exp.p.run,
+            trial=t,
+
+            iti=iti,
+            trial_time=trial_time,
+
+            motion=motion,
+            target=target,
+
+            responded=False,
+            response=np.nan,
+            correct=np.nan,
+            rt=np.nan,
+            result=np.nan,
+
+        )
+
+        return pd.Series(trial_info, dtype=np.object)
+
+
+def run_trial(exp, trial_info):
+
+    pass
---
+++ examples/simple_motion/params.py
@@ -0,0 +1,25 @@
+
+base = dict(
+
+    display_name="laptop",
+    display_luminance=50,
+
+    fix_radius=.15,
+    fix_window=2,
+
+    target_radius=.15,
+    target_window=3,
+
+    stim_sf=3,
+    stim_tex="sin",
+    stim_mask="gauss",
+    stim_size=3,
+    stim_contrast=.5,
+    stim_speed=3,
+
+    monitor_eye=True,
+
+    n_trials=50,
+
+
+)
