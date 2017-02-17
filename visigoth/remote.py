1a1dcfd08c8da8f11a4c6b1b017530aa4ecbf7b0
mwaskom <mwaskom@stanford.edu>
2017-02-17 16:43:16 -0500

    Copy over old code for layout of gaze control

--- visigoth/remote.py
+++ visigoth/remote.py
@@ -10,7 +10,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 from PyQt4.QtCore import Qt, QTimer
-from PyQt4.QtGui import (QApplication, QMainWindow,
+from PyQt4.QtGui import (QApplication, QMainWindow, QDialog,
                          QWidget, QSlider, QPushButton, QLabel,
                          QVBoxLayout, QHBoxLayout)
 
@@ -41,39 +41,103 @@ class RemoteApp(QMainWindow):
 
         self.main_frame = QWidget()
 
-        gaze_fig = self.initialize_gaze_figure()
+        gaze_app = GazeApp(self)
+
         trial_fig = self.initialize_trial_figure()
-
-        gaze_canvas = FigureCanvasQTAgg(gaze_fig)
         trial_canvas = FigureCanvasQTAgg(trial_fig)
-
-        gaze_canvas.setParent(self.main_frame)
         trial_canvas.setParent(self.main_frame)
-
-        gaze_vbox = QVBoxLayout()
-        gaze_vbox.addWidget(gaze_canvas)
 
         trial_vbox = QVBoxLayout()
         trial_vbox.addWidget(trial_canvas)
 
         main_hbox = QHBoxLayout()
-        main_hbox.addLayout(gaze_vbox)
+        main_hbox.addLayout(gaze_app.layout)
         main_hbox.addLayout(trial_vbox)
 
         self.main_frame.setLayout(main_hbox)
         self.setCentralWidget(self.main_frame)
 
 
-    def initialize_gaze_figure(self):
-
-        fig = Figure((5, 6))
-        ax = fig.add_subplot(111)
-        ax.set_aspect("equal")
-        print fig.axes
-        return fig
-
     def initialize_trial_figure(self):
 
-        fig = Figure((6, 5))
+        fig = Figure((5, 5), dpi=100, facecolor="white")
         ax = fig.add_subplot(111)
         return fig
+
+
+class GazeApp(object):
+
+    def __init__(self, remote_app):
+
+        self.remote_app = remote_app
+
+        fig = Figure((5, 5), dpi=100, facecolor="white")
+
+        ax = fig.add_subplot(111)
+        ax.set(xlim=(-10, 10),
+               ylim=(-10, 10),
+               aspect="equal")
+
+        ticks = np.linspace(-10, 10, 21)
+        ax.set_xticks(ticks, minor=True)
+        ax.set_yticks(ticks, minor=True)
+
+        grid_kws = dict(which="minor", lw=.5, ls="-", c=".8")
+        ax.xaxis.grid(True, **grid_kws)
+        ax.yaxis.grid(True, **grid_kws)
+
+        self.canvas = FigureCanvasQTAgg(fig)
+        self.canvas.setParent(remote_app.main_frame)
+
+        self.fix_label = QLabel("Fix window: 2.0")
+        self.fix_slider = QSlider(Qt.Horizontal)
+        self.fix_slider.setRange(0, 50)
+        #self.fix_slider.setValue(int(self.current_params["fix_radius"] * 10))
+        self.fix_slider.setTickPosition(QSlider.TicksBelow)
+        self.fix_slider.setTracking(True)
+        #self.fix_slider.valueChanged.connect(self.update_fix_radius)
+
+        self.x_label = QLabel("x offset: 0.0")
+        self.x_slider = QSlider(Qt.Horizontal)
+        self.x_slider.setRange(-40, 40)
+        #self.x_slider.setValue(int(self.current_params["x_offset"] * 10))
+        self.x_slider.setTickPosition(QSlider.TicksBelow)
+        self.x_slider.setTracking(True)
+        #self.x_slider.valueChanged.connect(self.update_x_offset)
+
+        self.y_label = QLabel("y offset: 0.0")
+        self.y_slider = QSlider(Qt.Horizontal)
+        self.y_slider.setRange(-40, 40)
+        #self.y_slider.setValue(int(self.current_params["y_offset"] * 10))
+        self.y_slider.setTickPosition(QSlider.TicksBelow)
+        self.y_slider.setTracking(True)
+        #self.y_slider.valueChanged.connect(self.update_y_offset)
+
+        self.update_button = QPushButton("Update")
+        #self.update_button.clicked.connect(self.update_params)
+
+        self.reset_button = QPushButton("Reset")
+        #self.reset_button.clicked.connect(self.reset_params)
+
+        controls = QHBoxLayout()
+
+        for (l, w) in [(self.fix_label, self.fix_slider),
+                       (self.x_label, self.x_slider),
+                       (self.y_label, self.y_slider)]:
+
+            vbox = QVBoxLayout()
+            vbox.addWidget(l)
+            vbox.addWidget(w)
+            vbox.setAlignment(w, Qt.AlignVCenter)
+            controls.addLayout(vbox)
+
+        vbox = QVBoxLayout()
+        vbox.addWidget(self.update_button)
+        vbox.addWidget(self.reset_button)
+        controls.addLayout(vbox)
+
+        vbox = QVBoxLayout()
+        vbox.addWidget(self.canvas)
+        vbox.addLayout(controls)
+
+        self.layout = vbox
