050c32248e541bd644acd54e63446aadd1d3a688
mwaskom <mwaskom@stanford.edu>
2017-03-03 16:18:53 -0500

    Import some objects nto the top-level namespace

--- visigoth/__init__.py
+++ visigoth/__init__.py
@@ -1,2 +1,7 @@
 import version
 __version__ = version.__version__
+
+from .experiment import Experiment
+from .remote import RemoteApp
+from .tools import AcquireFixation, AcquireTarget, flexible_values
+from .ext.bunch import Bunch
