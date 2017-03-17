dfed3fb5bdd5bec2023e9903d5f5da5bfa0edbf5
mwaskom <mwaskom@stanford.edu>
2017-03-17 16:16:52 -0400

    Mostly finish implementing RandomDotMotion stimulus

--- visigoth/stimuli/dots.py
+++ visigoth/stimuli/dots.py
@@ -1,3 +1,5 @@
+from __future__ import division
+import itertools
 import numpy as np
 
 from psychopy.visual import ElementArrayStim
@@ -6,57 +8,95 @@ from psychopy.visual import ElementArrayStim
 class RandomDotMotion(object):
 
     def __init__(self, win,
-                 shape="square", size=.05, color=1,
+                 shape="square", size=.1, color=1,
                  density=16.7, speed=5, interval=3,
-                 aperture_pos=(0, 0), aperture_size=5, elliptical=True,
+                 pos=(0, 0), aperture=5, elliptical=True,
                  ):
         """Classical Random Dot Motion stimulus ("Movshon noise").
 
         """
+        if np.isscalar(aperture):
+            aperture = [aperture] * 2
+        self.aperture = ax, ay = np.asarray(aperture)
+        self.elliptical = elliptical
+
+        self.norm = speed * interval / win.framerate
+        self.speed = speed
+        self.interval = interval
+        self.n_dots = int(np.round(density * ax * ay / win.framerate))
+        self.reset()
 
         shape = None if shape == "square" else shape
 
-        if np.isscalar(aperture_size):
-            aperture_size = [aperture_size] * 2 
-        self.aperture_size = ax, ay = np.asarray(aperture_size)
-
-        self.n_dots = int(np.round(density * ax * ay / win.framerate))
-
-        array = ElementArrayStim(win,
-                                 fieldPos=aperture_pos,
-                                 nElements=self.n_dots,
-                                 sizes=size,
-                                 colors=color,
-                                 elementMask=shape,
-                                 elementTex=None,
-                                 xys=self._random_xys(),
-                                 )
+        array = ElementArrayStim(
+            win,
+            fieldPos=pos,
+            nElements=self.n_dots,
+            sizes=size,
+            colors=color,
+            elementMask=shape,
+            elementTex=None,
+            xys=next(self.dotpos),
+            )
 
         self.array = array
-                                 
 
     def _random_xys(self, n=None):
-
+        """Generate random dot positions within the stimulus aperature."""
+        # TODO allow specified random seed
         if n is None:
             n = self.n_dots
 
-        # TODO allow set random seed
-        halfx = self.aperture_size[1] / 2.0
+        halfx, halfy = self.aperture / 2
         x = np.random.uniform(-halfx, halfx, n)
-        
-        halfy = self.aperture_size[0] / 2.0
         y = np.random.uniform(-halfy, halfy, n)
 
         return np.column_stack([x, y])
 
     def reset(self):
-
-        pass
+        """Generate random starting positions for each set of dots."""
+        self.dotpos = itertools.cycle(
+            [self._random_xys() for _ in range(self.interval)]
+            )
 
     def update(self, direction, coherence):
+        """Advance the dot animation one frame."""
+        # Get the dots to be drawn on the next frame
+        xys = next(self.dotpos)
 
-        self.array.xys = self._random_xys()
+        # Identify signal dots
+        signal = np.random.rand(self.n_dots) < coherence
+
+        # Displace the signal dots
+        theta = direction / 180 * np.pi
+        dxdy = np.array([[np.cos(theta), -np.sin(theta)]]) * self.norm
+        xys[signal] += dxdy
+
+        # Randomly reposition the noise dots
+        xys[~signal] = self._random_xys(np.sum(~signal))
+
+        # Wrap-around dots that were displaced out of bounds
+        oob = np.any(np.abs(xys) > (self.aperture / 2), axis=1)
+        rotmat = np.array([[np.cos(theta), -np.sin(theta)],
+                           [np.sin(theta), np.cos(theta)]])
+        refmat = np.array([[-1, 0], [0, -1]])
+        xfm = np.linalg.inv(rotmat).dot(refmat).dot(rotmat)
+        xys[oob] = np.dot(xys[oob] - dxdy, xfm)
+
+        # Identify dots in the corners of an elliptical aperture
+        x, y = xys.T
+        if self.elliptical:
+            a, b = (self.aperture / 2) ** 2
+            show = (x ** 2 / a + y ** 2 / b) < 1
+        else:
+            show = np.ones(self.n_dots)
+
+        # Update the Psychopy object
+        self.array.xys = xys
+        self.array.opacities = show.astype(float)
+
+        # TODO log dot position somewhere in this object
 
     def draw(self):
-
+        """Draw the Psychopy object to the window."""
         self.array.draw()
