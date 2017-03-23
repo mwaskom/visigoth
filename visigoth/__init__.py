0f75b49629cf97bcde8ba392604e22c083da8d9f
mwaskom <mwaskom@stanford.edu>
2017-03-23 16:52:29 -0400

    Don't import RemoteApp at top level
    This means that visigoth core does not require a matplotlib import

--- visigoth/__init__.py
+++ visigoth/__init__.py
@@ -2,6 +2,5 @@ import version
 __version__ = version.__version__
 
 from .experiment import Experiment
-from .remote import RemoteApp
 from .tools import AcquireFixation, AcquireTarget, flexible_values
 from .ext.bunch import Bunch
