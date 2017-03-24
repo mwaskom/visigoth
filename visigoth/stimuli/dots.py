d0d03cb2c11294b9bc9967e6c75f5ed513bbe284
mwaskom <mwaskom@stanford.edu>
2017-03-24 16:03:02 -0400

    Add RandomDotColorMotion stimulus

--- visigoth/stimuli/dots.py
+++ visigoth/stimuli/dots.py
@@ -1,6 +1,7 @@
 from __future__ import division
 import itertools
 import numpy as np
+from colorspacious import cspace_converter
 
 from psychopy.visual import ElementArrayStim
 
@@ -53,14 +54,8 @@ class RandomDotMotion(object):
 
         return np.column_stack([x, y])
 
-    def reset(self):
-        """Generate random starting positions for each set of dots."""
-        self.dotpos = itertools.cycle(
-            [self._random_xys() for _ in range(self.interval)]
-            )
+    def _update_positions(self, direction, coherence):
 
-    def update(self, direction, coherence):
-        """Advance the dot animation one frame."""
         # Get the dots to be drawn on the next frame
         xys = next(self.dotpos)
 
@@ -97,6 +92,80 @@ class RandomDotMotion(object):
 
         # TODO log dot position somewhere in this object
 
+    def reset(self):
+        """Generate random starting positions for each set of dots."""
+        self.dotpos = itertools.cycle(
+            [self._random_xys() for _ in range(self.interval)]
+            )
+
+    def update(self, direction, coherence):
+        """Advance the dot animation one frame."""
+        self._update_positions(direction, coherence)
+
     def draw(self):
         """Draw the Psychopy object to the window."""
         self.array.draw()
+
+
+class RandomDotColorMotion(RandomDotMotion):
+
+    def __init__(self, win,
+                 shape="square", size=.05,
+                 density=16.7, speed=5, interval=3,
+                 lightness=50, chromacity=50,
+                 pos=(0, 0), aperture=5, elliptical=True,
+                 ):
+        """Classical random dot motion stimulus with randomly colored dots.
+
+        """
+        init_color = 1
+        super(RandomDotColorMotion, self).__init__(
+            win, shape, size, init_color, density, speed, interval,
+            pos, aperture, elliptical,
+            )
+        self.lightness = lightness
+        self.chromacity = chromacity
+        self.jch_to_rgb = cspace_converter("JCh", "sRGB1")
+
+    def jch_to_psychopy_rgb(self, jch):
+        """Convert JCh colors to RGB in [-1, 1]."""
+        return np.clip(self.jch_to_rgb(jch), 0, 1) * 2 - 1
+
+    def _random_rgbs(self, n=None):
+        """Generate random PsychoPy RGB hues with fixed J/C.
+
+        This function will clip RGB values outside of [0, 1]. It's up to the
+        user to pick lightness/chromacity values that will keep all colors in
+        the gamut if they need exact pairwise color distances.
+
+        """
+        # TODO fixed random seed
+        if n is None:
+            n = self.n_dots
+
+        j = np.ones(n) * self.lightness
+        c = np.ones(n) * self.chromacity
+        h = np.random.uniform(0, 360, n)
+        jch = np.c_[j, c, h]
+        rgbs = self.jch_to_psychopy_rgb(jch)
+
+        return rgbs
+
+    def _update_colors(self, hue, coherence):
+
+        # Identify signal dots
+        signal = np.random.rand(self.n_dots) < coherence
+
+        # Generate dot colors
+        rgbs = self._random_rgbs()
+        signal_jch = self.lightness, self.chromacity, hue
+        signal_rgb = self.jch_to_psychopy_rgb(signal_jch)
+        rgbs[signal] = signal_rgb
+
+        # Update the Psychopy object
+        self.array.colors = rgbs
+
+    def update(self, direction, motion_coherence, hue, color_coherence):
+        """Advance the dot animation one frame."""
+        self._update_positions(direction, motion_coherence)
+        self._update_colors(hue, color_coherence)
