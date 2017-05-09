5598eed73ecd24284d0a040a07f198fb8185e50e
mwaskom <mwaskom@stanford.edu>
2017-05-09 14:06:05 -0400

    Add PointCue class

--- visigoth/stimuli/__init__.py
+++ visigoth/stimuli/__init__.py
@@ -1,5 +1,5 @@
 from points import Point, Points
-from cue import LineCue
+from cue import LineCue, PointCue
 from grating import Grating
 from elementarray import ElementArray
 from pattern import Pattern
--- visigoth/stimuli/cue.py
+++ visigoth/stimuli/cue.py
@@ -1,15 +1,18 @@
+# TODO BOTH CLASSES HERE NEED DOCUMENTATION
 import numpy as np
 from psychopy import visual
 
 
 class LineCue(object):
 
-    def __init__(self, win, extent=(0, 1), width=5, color=1):
+    def __init__(self, win, extent=(0, 1), width=5, color=1, **kwargs):
 
         self.extent = extent
         stim = visual.Line(win,
                            lineWidth=width,
-                           lineColor=color)
+                           lineColor=color,
+                           autoLog=False,
+                           **kwargs)
 
         self.stim = stim
 
@@ -26,6 +29,38 @@ class LineCue(object):
         direction = val / np.linalg.norm(val)
         self.stim.start = direction * self.extent[0]
         self.stim.end = direction * self.extent[1]
+
+    def draw(self):
+
+        self.stim.draw()
+
+
+class PointCue(object):
+
+    def __init__(self, win, norm, radius, color=1, **kwargs):
+
+        self.norm = norm
+        stim = visual.Circle(win,
+                             radius=radius,
+                             fillColor=color,
+                             lineColor=color,
+                             interpolate=True,
+                             autoLog=False,
+                             **kwargs)
+        self.stim = stim
+
+    @property
+    def pos(self):
+
+        return self._pos
+
+    @pos.setter
+    def pos(self, val):
+
+        self._pos = val
+        val = np.asarray(val)
+        direction = val / np.linalg.norm(val)
+        self.stim.pos = direction * self.norm
 
     def draw(self):
 
